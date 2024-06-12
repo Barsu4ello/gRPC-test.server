af8319e4d03502e207810b70d0d8ad98c2396d64
Gorbunov Vladislav <wint-mil@mail.ru>
2024-06-12 16:29:32 +0300

    Common repo.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class App 
 {
     public static void main( String[] args ) throws IOException, InterruptedException {
+
         Server server = ServerBuilder
                 .forPort(8080)
                 .addService(new GreetingServiceImpl())
